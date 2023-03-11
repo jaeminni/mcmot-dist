8354e26b87ed3765616522013f877350392acdcf
jaeminni <jaeminni@42dot.ai>
2023-03-12 01:04:04 +0900

    init

---
+++ src/main/js/index.js
@@ -0,0 +1,14 @@
+import {MvOptions, MvCamera, MvFrame, MvScene, MvProject} from "./MvObjects"
+import {MvController, MvApplication} from "./MvApplication";
+import GLController from "./GLController";
+
+window.MvOptions = MvOptions
+window.MvCamera = MvCamera
+window.MvFrame = MvFrame
+window.MvScene = MvScene
+window.MvProject = MvProject
+
+window.MvController = MvController
+window.MvApplication = MvApplication
+
+window.GLController = GLController
